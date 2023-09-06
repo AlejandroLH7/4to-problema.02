c41cc9988b94d1b726262048bd2de7296ed6de2a
Alejandro Ledesma Huayan <143223151+AlejandroLH7@users.noreply.github.com>
2023-09-05 23:36:58 -0500

    Agregar archivos de proyecto.

---
+++ 4to problema/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4to_problema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, cantidad, hora, minuto;
+            Console.WriteLine("Ingrese la cantidad de examenes A:");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de examenes B:");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de examenes C:");
+            C = int.Parse(Console.ReadLine());
+
+            cantidad = (A * 5) + (B * 8) + (C * 6);
+            hora = cantidad / 60;
+            minuto = cantidad % 60;
+            Console.WriteLine("=============================");
+            Console.WriteLine("tardara " + hora + " horas y " + minuto + " minutos en finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
